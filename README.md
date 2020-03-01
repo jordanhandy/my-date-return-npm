339b0cb4e7f10686f9c1a8872f5b2d43246497ee
Jordan Handy <handy.jordan@gmail.com>
2020-03-01 15:06:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# My-Date-Return
+Testing npm Package creation.
+The functions in this package take no arguments.
+
+## Usage
+### Installation
+```
+var myDateReturn = require("my-date-return");
+``` 
+### Calling
+myDateReturn.getFullDate(); - returns full date with day, month and year
+myDateReturn.getDay(); - returns simply the day of the week
+
---
+++ my-date-return.js
@@ -0,0 +1,28 @@
+// Date manipulator module.
+
+module.exports.getFullDate = function() {
+
+    const today = new Date();
+  
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+    };
+  
+    return today.toLocaleDateString("en-US", options);
+  
+  }
+  
+  module.exports.getDay = function () {
+  
+    const today = new Date();
+  
+    const options = {
+      weekday: "long"
+    };
+  
+    return today.toLocaleDateString("en-US", options);
+  
+  }
+  
